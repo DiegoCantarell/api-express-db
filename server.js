7f1a7723cdfe92d940d0614f56909fc4af1de983
kantrax <diegogccantarell@gmail.com>
2022-07-11 14:28:32 -0500

    Adding to server.js -> endpoint PUT to create new explorers

--- server.js
+++ server.js
@@ -29,6 +29,20 @@ app.post('/explorers', async (req, res) => {
   await prisma.explorer.create({data: explorer});
   return res.json({message});
 });
+app.put('/explorers/:id', async (req, res) => {
+	const id = parseInt(req.params.id);
+
+	await prisma.explorer.update({
+		where: {
+			id: id
+		},
+		data: {
+			mission: req.body.mission
+		}
+	})
+
+	return res.json({message: "Actualizado correctamente"});
+});
 app.listen(port, () => {
   console.log(`Listening to requests on port ${port}`);
 });
